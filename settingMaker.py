173ce0fb53127c3b912915f5d96f7d4115ac9d78
miklast <mdieatrick@gmail.com>
2022-02-03 13:19:16 -0800

    changed logic for seeing if settings.ini needs to be created

--- settingMaker.py
+++ settingMaker.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from os.path import exists as file_exists
 
 # function for the settings creation and checker, called every time the main program is run to make sure a settings file always exists
 def configSetup():
@@ -7,11 +8,12 @@ def configSetup():
     config.read('settings.ini')
 
     #check for settings.ini. If it exists, we good. Otherwise, create with these default values.
-    if 'main' in config:
+    if file_exists('settings.ini'):
         print("settings.ini already exists!")
 
-    elif 'main' not in config:
+    else:
         config.add_section('main')
+        #Need to make a way to ask the user for key on first time startup
         config.set('main', 'TBA-KEY', '')
         config.set('main', "rounding",'2')
         config.set('main',"stop-time",'35')
